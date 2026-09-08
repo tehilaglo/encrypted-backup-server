74c6edc8681b846c853fd71aa22d0d7479089ac7
Tehila <glotehila@gmail.com>
2026-09-08 14:02:27 +0300

    Update cleanup script to remove server and port info files

--- cleanup.sh
+++ cleanup.sh
@@ -8,8 +8,6 @@ FILES_DIR="$PROJECT_ROOT/server"
 
 # Define the file to be removed
 DB_FILE="$FILES_DIR/clients.db"
-SRV_INFO="$FILES_DIR/server.info"
-PORT_INFO="$FILES_DIR/port.info"
 
 # Check if the file exists in the current directory and remove it
 if [ -f "$DB_FILE" ]; then
@@ -17,20 +15,6 @@ if [ -f "$DB_FILE" ]; then
     echo "$DB_FILE removed."
 else
     echo "$DB_FILE not found."
-fi
-
-if [ -f "$SRV_INFO" ]; then
-    rm "$SRV_INFO"
-    echo "$SRV_INFO removed."
-else
-    echo "$SRV_INFO not found."
-fi
-
-if [ -f "$PORT_INFO" ]; then
-    rm "$PORT_INFO"
-    echo "$PORT_INFO removed."
-else
-    echo "$PORT_INFO not found."
 fi
 
 echo "Cleanup complete."
